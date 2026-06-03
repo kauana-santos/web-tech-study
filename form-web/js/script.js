4b0b5ace3bf9a4ddc12f56eeb7bd797a8ceeaa19
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-03 11:48:59 -0300

    adicionando novos campos

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -1,15 +1,26 @@
 const nome = document.querySelector("#nome");
 const button = document.querySelector("button");
 const lista = document.querySelector(".lista");
+const email = document.querySelector("#email");
+const telefone = document.querySelector("#telefone");
 
 button.addEventListener("click", (event) =>{
     event.preventDefault();
     const inputValue = nome.value;
-    const templateHTML = `<li>${inputValue}</li>`;
+    const emailValue = email.value;
+    const telefoneValue = telefone.value;
+
+    const templateHTML = `<li>
+        <h2>${inputValue}</h2>
+        <p>Email: ${emailValue} <br>Telefone: ${telefoneValue}</p>
+    </li>`;
     
-    console.log(inputValue);
+    console.log( telefone);
+    console.log(email);
 
     lista.innerHTML += templateHTML;
     
     nome.value = "";
+    email.value= "";
+    telefone.value="";
 })
