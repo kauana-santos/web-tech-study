da3c52b7d58f0b87587645f37c3214313d34edd2
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-03 11:16:00 -0300

    feat: finalizando layout do css e adicionando js no projeto

---
+++ form-web/js/script.js
@@ -0,0 +1,9 @@
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+button.addEventListener("click", (event) =>{
+    event.preventDefault();
+    console.log(nome.value);
+
+})
