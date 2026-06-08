4168137f323b2e26a28f45137590867725fe7dcd
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-08 11:21:03 -0300

    feat: adicionando código

---
+++ exemplo-js/calc.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 10;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log("Soma: " + soma);
+console.log("Subtração: " + subtracao);
+console.log("Multiplicação: " + multiplicacao);
+console.log("Divisão: " + divisao);
