74cd3e98c45aed93c9e87527fa179f50b748ae3b
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-08 10:46:03 -0300

    feat: adicionando código

---
+++ exemplo-js/tipo-dadp.js
@@ -0,0 +1,19 @@
+//String
+let nome = "Kauana";
+
+//Inteiro
+let idade = 20;
+
+//Booleano
+let casada = false;
+
+//números flutuantes
+let valorProduto = 5.35;
+
+//Resultados
+console.log("Meu nome é: " + nome);
+console.log("Minha idade é: " + idade);
+console.log("Casada? " + casada);
+console.log("R$ " + valorProduto);
+
+console.log(typeof nome);
