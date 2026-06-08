5e9f53ab07279b3dc82749d57631ddcb07445f24
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-08 11:49:47 -0300

    feat: adicionando funções em js

--- exemplo-js/calcNew.js
+++ exemplo-js/calcNew.js
@@ -1,6 +1,28 @@
-function meuNome(nome){
+// function meuNome(nome){
 
-    console.log(nome);
+//     console.log(nome);
+// }
+
+// meuNome("kauana");
+
+function soma(num1, num2){
+    console.log( num1 + num2);
 }
 
-meuNome("kauana");
+function sub(num1, num2){
+    console.log( num1 - num2);
+
+}
+function mult(num1, num2){
+    console.log( num1 * num2);
+
+}
+function div(num1, num2){
+    console.log( num1 / num2);
+
+}
+
+soma(10, 3);
+sub(5, 5);
+mult(10, 3);
+div(10, 3);
