0abeb03d4b093b373b187af87adf5a6f8e60aafe
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-10 08:45:35 -0300

    feat: exemplos de funções

---
+++ exemplo-js/funcoes/func.js
@@ -0,0 +1,11 @@
+function minhaFuncao(){
+    document.write("Minha função");
+}
+
+function mostrarNome(nome){
+    document.write(nome)
+}
+
+minhaFuncao();
+mostrarNome("Kauana");
+mostrarNome("Bruna");
