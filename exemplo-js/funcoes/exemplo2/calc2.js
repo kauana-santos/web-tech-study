d865763d17c4645c3dd32bae23a9701401852087
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-10 09:10:47 -0300

    feat: alterações no calc2

---
+++ exemplo-js/funcoes/exemplo2/calc2.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("digite um numero"))
+let num2 = Number(prompt("digite um outro numero"))
+
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let div = num1 / num2
+
+console.log(soma);
+console.log("Subtração: " + sub);
+console.log("Multiplicação: " + mult);
+console.log("Divisão: " + div);
