98bd3af47bd5394a712653c71b393b9d71db4a5b
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-06-15 11:36:12 -0300

    feat: mudando estilo popup

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -1,18 +1,20 @@
 const btn = document.querySelector("button");
 const popup = document.querySelector(".popup-wrapper");
 
-
 btn.addEventListener("click", (event) => {
     popup.classList.add("d-block")
+    
 })
 
 
 popup.addEventListener("click", (event) => {
     const classOfClickElement = event.target.classList[0];
-    const classNameListArray =["popup-close", "popup-link", "popup-wrapper"]
-    console.log(classNameListArray.includes(classOfClickElement))
+    const listaDeClasses = event.target.matches(".popup-close, .popup-link, .popup-wrapper");
 
-    if(classNameListArray.includes(classOfClickElement) == true)
+    if(listaDeClasses == true){
         popup.classList.remove("d-block")
+        
+    }
+        
 })
 
