d377196d2149a348a4dbca2abc243ddf092d043e
KAUANA PEREIRA DOS SANTOS RODRIGUES <kauana.rodrigues@senaisp.edu.br>
2026-06-17 10:45:09 -0300

    feat: adicionando códigos par salvar no localStorage

---
+++ cadastro-local/js/script.js
@@ -0,0 +1,23 @@
+const campo = document.querySelector("#campo");
+const salvarBtn = document.querySelector("#salvar");
+const lista = document .querySelector("#listaValores")
+
+salvarBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const valor = campo.value;
+
+    console.log(valor)
+
+    if(!valor.trim()){
+        alert("O campo não pode estar vazio");
+    }
+
+    //limpar campo
+    campo.value = "";
+
+    //salvar no localStorage
+    const chave = `ValorSalvo_${Date.now()}`;
+    localStorage.setItem(chave, valor)
+
+
+})
