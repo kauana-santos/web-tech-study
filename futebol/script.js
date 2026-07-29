c1ded84544aceff9e8b6b726c47e9cf0a1f89ffc
kauana-sants <kauanasantos815@gmail.com>
2026-07-29 08:57:18 -0300

    codigo da aula

---
+++ futebol/script.js
@@ -0,0 +1,18 @@
+const jogadores = [
+    "Neymar",
+    "Vinicius Júnior",
+    "Rodrygo",
+    "Endrick",
+    "Casemiro",
+    "Alisson",
+    "Marquinhos",
+    "Gabriel Magalhães",
+    "Bruno Guimarães",
+    "Raphinha"
+];
+
+console.log("111")
+
+function listarJogadores(){
+    alert("1")
+}
